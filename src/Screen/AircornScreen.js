c942acb7c27539302e0c317ed6c03b763044d179
tjdgns8047 <tjdgns8047@naver.com>
2017-07-19 05:19:47 +0900

    오류>>>>

--- src/Screen/AircornScreen.js
+++ src/Screen/AircornScreen.js
@@ -2,21 +2,58 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import styled from 'styled-components/native';
 import { Actions } from 'react-native-router-flux'
+import { TabViewAnimated, TabBar, SceneMap } from 'react-native-tab-view';
+
+const FirstRoute = () => <View style={[ styles.container, { backgroundColor: '#ff4081' } ]} />;
+const SecondRoute = () => <View style={[ styles.container, { backgroundColor: '#673ab7' } ]} />;
 
 export default class AircornScreen extends React.Component {
+
+  state = {
+    index: 0,
+    routes: [
+      { key: '1', title: 'First' },
+      { key: '2', title: 'Second' },
+    ],
+  };
+
+  _handleChangeTab = index => this.setState({ index });
+
+  _renderHeader = props => <TabBar {...props} />;
+
+  _renderScene = SceneMap({
+    '1': FirstRoute,
+    '2': SecondRoute,
+  });
+
   render() {
     return (
+      
+      <TabViewAnimated
+        style={styles.container}
+        navigationState={this.state}
+        renderScene={this._renderScene}
+        renderHeader={this._renderHeader}
+        onRequestChangeTab={this._handleChangeTab}
+      />
+
       <StyledView>
         <StyledText
           onPress={() => Actions.home()}
           >
-          AircornScreen
           {this.props.text}
         </StyledText>
       </StyledView>
+
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 const StyledView = styled.View`
   flex: 1;
