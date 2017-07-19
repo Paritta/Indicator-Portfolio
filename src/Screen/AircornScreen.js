04e09f05e268955e156ff9efa6b87b4368a51513
tjdgns8047 <tjdgns8047@naver.com>
2017-07-19 10:29:35 +0900

    텝바 시작

--- src/Screen/AircornScreen.js
+++ src/Screen/AircornScreen.js
@@ -2,13 +2,9 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import styled from 'styled-components/native';
 import { Actions } from 'react-native-router-flux'
-import { TabViewAnimated, TabBar, SceneMap } from 'react-native-tab-view';
-
-const FirstRoute = () => <View style={[ styles.container, { backgroundColor: '#ff4081' } ]} />;
-const SecondRoute = () => <View style={[ styles.container, { backgroundColor: '#673ab7' } ]} />;
+import { TabViewAnimated, TabBar } from 'react-native-tab-view';
 
 export default class AircornScreen extends React.Component {
-
   state = {
     index: 0,
     routes: [
@@ -17,54 +13,59 @@ export default class AircornScreen extends React.Component {
     ],
   };
 
-  _handleChangeTab = index => this.setState({ index });
+  _handleChangeTab = (index) => {
+    this.setState({ index });
+  };
 
-  _renderHeader = props => <TabBar {...props} />;
+  _renderHeader = (props) => {
+    return <TabBar {...props} />;
+  };
 
-  _renderScene = SceneMap({
-    '1': FirstRoute,
-    '2': SecondRoute,
-  });
-
+  _renderScene = ({ route }) => {
+    switch (route.key) {
+    case '1':
+      return <StyledView style={[ styles.page, { backgroundColor: 'white' } ]} >
+                <StyledText>This is Timer</StyledText>
+              </StyledView>
+        ;
+    case '2':
+      return <View style={[ styles.page, { backgroundColor: '#673ab7' } ]} />;
+    default:
+      return null;
+    }
+  };
   render() {
     return (
-      
-      <TabViewAnimated
-        style={styles.container}
-        navigationState={this.state}
-        renderScene={this._renderScene}
-        renderHeader={this._renderHeader}
-        onRequestChangeTab={this._handleChangeTab}
-      />
-
       <StyledView>
-        <StyledText
-          onPress={() => Actions.home()}
-          >
-          {this.props.text}
-        </StyledText>
+        <TabViewAnimated
+          style={styles.container}
+          navigationState={this.state}
+          renderScene={this._renderScene}
+          renderHeader={this._renderHeader}
+          onRequestChangeTab={this._handleChangeTab}
+        />
       </StyledView>
 
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
-
 const StyledView = styled.View`
   flex: 1;
-  background-color: deeppink;
-  opacity: 0.6;
-  align-items: center;
-  justify-content: center;
 `
 
 const StyledText = styled.Text`
   font-size: 20px;
   text-align: center;
-  color: white;
+  color: black;
 `
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  page: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
